47e692648f5c40ce2e82a0f9b8be0d807f127ff5
gauravsak <gauravs841990@gmail.com>
2014-05-26 18:02:04 +0530

    Removed commented code

--- src/main/java/com/ee/todoApp/db/RedisDbUtil.java
+++ src/main/java/com/ee/todoApp/db/RedisDbUtil.java
@@ -1,6 +1,5 @@
 package com.ee.todoApp.db;
 
-import java.util.HashSet;
 import java.util.Set;
 
 import redis.clients.jedis.Jedis;
@@ -11,18 +10,12 @@ public class RedisDbUtil {
 	
 	public static void set(final String key, final String value) {
 		jedis = new Jedis("localhost");
-//		jedis.set("todo:" + todo.getId() + ":note", todo.getNote());
 		jedis.set(key, value);
 		jedis.close();
 	}
 	
 	public static String get(final String key) {
 		jedis = new Jedis("localhost");
-//		Todo todo = null;
-//		String note = jedis.get("todo:" + id + ":note");
-//		if(note != null) {
-//			todo = new Todo(id, note);
-//		}
 		String value = jedis.get(key);
 		jedis.close();
 		return value;
@@ -30,13 +23,6 @@ public class RedisDbUtil {
 	
 	public static Set<String> getKeys(String wildcardPattern) {
 		jedis = new Jedis("localhost");
-//		Set<String> todoNoteKeys = jedis.keys("todo:*:note");
-//		Set<Todo> allTodos = new HashSet<>();
-//		for (String todoNoteKey : todoNoteKeys) {
-//			int id = Integer.parseInt(todoNoteKey.split(":")[1]);
-//			String note = jedis.get(todoNoteKey);
-//			allTodos.add(new Todo(id, note));
-//		}
 		Set<String> keys = jedis.keys(wildcardPattern);
 		jedis.close();
 		return keys;
@@ -44,10 +30,6 @@ public class RedisDbUtil {
 	
 	public static void delete(final String key) {
 		jedis = new Jedis("localhost");
-//		String note = jedis.get("todo:" + id + ":note");
-//		if(note != null) {
-//			jedis.del(note);
-//		}
 		String value = jedis.get(key);
 		if(value != null) {
 			jedis.del(key);
@@ -57,10 +39,6 @@ public class RedisDbUtil {
 	
 	public static void update(final String key, final String value) {
 		jedis = new Jedis("localhost");
-//		String todoNoteKey = "todo:" + todo.getId() + ":note";
-//		if(jedis.get(todoNoteKey) != null) {
-//			jedis.set(todoNoteKey, todo.getNote());
-//		}
 		if(jedis.get(key) != null) {
 			jedis.set(key, value);
 		}
